7abf6bb54ef6e83e614c0867753d4cffe8cee137
Documenter.jl <documenter@juliadocs.github.io>
2025-09-22 18:07:50 +0000

    build based on 43b0438

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Quoll","page":"Home","title":"Quoll","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Quoll.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
