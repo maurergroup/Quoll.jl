33d8ddfed47bc05e5dfe18c867a8f4730731d1df
Documenter.jl <documenter@juliadocs.github.io>
2025-09-24 15:47:17 +0000

    build based on 86a6fda

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#Quoll","page":"Home","title":"Quoll","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Quoll.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+[{"location":"#Quoll","page":"Home","title":"Quoll","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Quoll.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"#Quoll.Parser.read_inputfile-Tuple{AbstractString}","page":"Home","title":"Quoll.Parser.read_inputfile","text":"read_inputfile(filepath::AbstractString)\n\nRead Quoll input file in TOML format.\n\n\n\n\n\n","category":"method"}]
 }
