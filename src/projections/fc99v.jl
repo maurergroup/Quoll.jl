f85dacd8ea7e36853d94e8d9682bd1e63f72cb11
Valdas Vitartas <valdas.vitartas@warwick.ac.uk>
2025-12-01 20:27:54 +0000

    Optimise core projection with BLAS

--- src/projections/fc99v.jl
+++ src/projections/fc99v.jl
@@ -13,20 +13,28 @@ function compute_valence_operator_data(operators, core_mask_list, valence_mask_l
     core_mask_overlap = core_mask_list[first(i_overlap)]
     valence_mask_overlap = valence_mask_list[first(i_overlap)]
 
-    _, S₁₂, _ = core_valence_partition(overlap, core_mask_overlap, valence_mask_overlap)
+    S₁₂ = get_data(overlap)[core_mask_overlap, valence_mask_overlap]
     
     return [
-        compute_valence_operator_data(operator, S₁₂, core_mask, valence_mask, method)
+        compute_valence_operator_data(operator, S₁₂, core_mask, valence_mask, method, get_float(operator))
         for (operator, core_mask, valence_mask) in zip(operators, core_mask_list, valence_mask_list)
     ]
 end
 
 function compute_valence_operator_data(operator::DenseOperator, S₁₂::AbstractMatrix, core_mask::BitVector,
-    valence_mask::BitVector, ::FC99V)
+    valence_mask::BitVector, ::FC99V, ::Type{T}) where T<:Complex
 
-    O₁₁, O₁₂, O₂₂ = core_valence_partition(operator, core_mask, valence_mask)
+    O₁₁ = get_data(operator)[core_mask, core_mask]
+    O₁₂ = get_data(operator)[core_mask, valence_mask]
+    O₂₂ = get_data(operator)[valence_mask, valence_mask]
 
-    A₂₂ = (0.5 * S₁₂' * O₁₁ - O₁₂') * S₁₂
+    α = T(0.5)
+    β = T(-1.0)
+    LinearAlgebra.BLAS.hemm!('L', 'U', α, O₁₁, S₁₂, β, O₁₂)
+
+    # A₂₂ = S₁₂' * (0.5 * O₁₁ * S₁₂ - O₁₂)
+    A₂₂ = LinearAlgebra.BLAS.gemm('C', 'N', S₁₂, O₁₂)
+
     Ō₂₂ = O₂₂ + A₂₂ + A₂₂'
 
     return Ō₂₂
--- src/projections/laikov.jl
+++ src/projections/laikov.jl
@@ -14,22 +14,35 @@ function compute_valence_operator_data(operators, core_mask_list, valence_mask_l
     valence_mask_overlap = valence_mask_list[first(i_overlap)]
 
     # Precompute core-core overlap inverse
-    S₁₁, S₁₂, _ = core_valence_partition(overlap, core_mask_overlap, valence_mask_overlap)
+    S₁₁ = get_data(overlap)[core_mask_overlap, core_mask_overlap]
+    S₁₂ = get_data(overlap)[core_mask_overlap, valence_mask_overlap]
     S₁₁⁻¹ = inv(S₁₁)
     
     return [
-        compute_valence_operator_data(operator, S₁₁⁻¹, S₁₂, core_mask, valence_mask, method)
+        compute_valence_operator_data(operator, S₁₁⁻¹, S₁₂, core_mask, valence_mask, method, get_float(operator))
         for (operator, core_mask, valence_mask) in zip(operators, core_mask_list, valence_mask_list)
     ]
 end
 
+# TODO: could use the same function if used abstract type of laikov and FC99V
+# but would have to move Ŝ₁₂ outside
 function compute_valence_operator_data(operator::DenseOperator, S₁₁⁻¹::AbstractMatrix, S₁₂::AbstractMatrix,
-    core_mask::BitVector, valence_mask::BitVector, ::LaikovCore)
+    core_mask::BitVector, valence_mask::BitVector, ::LaikovCore, ::Type{T}) where T<:Complex
 
-    O₁₁, O₁₂, O₂₂ = core_valence_partition(operator, core_mask, valence_mask)
+    O₁₁ = get_data(operator)[core_mask, core_mask]
+    O₁₂ = get_data(operator)[core_mask, valence_mask]
+    O₂₂ = get_data(operator)[valence_mask, valence_mask]
 
-    Ŝ₁₂ = S₁₁⁻¹ * S₁₂
-    A₂₂ = (0.5 * Ŝ₁₂' * O₁₁ - O₁₂') * Ŝ₁₂
+    # Ŝ₁₂ = S₁₁⁻¹ * S₁₂
+    Ŝ₁₂ = LinearAlgebra.BLAS.hemm('L', 'U', S₁₁⁻¹, S₁₂)
+
+    α = T(0.5)
+    β = T(-1.0)
+    LinearAlgebra.BLAS.hemm!('L', 'U', α, O₁₁, Ŝ₁₂, β, O₁₂)
+
+    # A₂₂ = Ŝ₁₂' * (0.5 * O₁₁ * Ŝ₁₂ - O₁₂)
+    A₂₂ = LinearAlgebra.BLAS.gemm('C', 'N', Ŝ₁₂, O₁₂)
+
     Ō₂₂ = O₂₂ + A₂₂ + A₂₂'
 
     return Ō₂₂
