236e8306df837f09ebbd334eab0096d2a931bb1b
Valdas Vitartas <valdas.vitartas@warwick.ac.uk>
2026-04-12 17:24:46 +0100

    Change example naming

--- test/regression/regressiontests.jl
+++ test/regression/regressiontests.jl
@@ -1,7 +1,13 @@
 using SafeTestsets
 
-# TODO: Could separate into two different tests when RealBSparse
-# contains write methods
-@safetestset "SiC FHIaimsCSC -> RealBSparse -> DeepH" begin
-    include("SiC_FHIaimsCSC_DeepH.jl")
+@safetestset "Silicon carbide FHIaimsCSC -> CanonicalBlock -> DeepHBlock" begin
+    include("fhi_canon_deeph_siliconcarbide.jl")
 end
+
+# @safetestset "Gold FHIaimsCSC -> CanonicalBlock -Laikov-> CanonicalBlock -> DeepHBlock" begin
+#     include("fhi_canon_laikov_deeph_gold.jl")
+# end
+
+# @safetestset "N-doped graphene DeepHBlock -> CanonicalBlock -> CanonicalDenseRecip" begin
+#     include("deeph_canon_canonrecip_dopedgraphene.jl")
+# end
